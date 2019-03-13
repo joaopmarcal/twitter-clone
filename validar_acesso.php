54c7249372dd675d03587146093c37f65a881d18
João Paulo <paulospc21@gmail.com>
2019-03-13 20:22:17 -0300

    feito aula 285

---
+++ validar_acesso.php
@@ -0,0 +1,30 @@
+<?php
+
+    require_once('db.class.php');
+
+    $usuario = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha' ";
+
+    $objDb = new db();
+    $link = $objDb->conecta_mysql();
+
+    $resultado_id = mysqli_query($link, $sql);
+
+    if($resultado_id){
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+        var_dump($dados_usuario);
+    } else {
+        echo 'Erro na execução da consulta, favor entrar em contato com o admin do site';
+    }
+
+
+
+    //update true/false
+    //insert true/false
+    //select false/resource
+    //delete true/false
+
+
+?>
