b48c4a314903f66ba49f7be776a379806739fc2d
João Paulo <paulospc21@gmail.com>
2019-03-13 20:49:38 -0300

    feito aula 286

--- validar_acesso.php
+++ validar_acesso.php
@@ -14,7 +14,11 @@
 
     if($resultado_id){
         $dados_usuario = mysqli_fetch_array($resultado_id);
-        var_dump($dados_usuario);
+        if(isset($dados_usuario['usuario'])){
+            echo 'Usuario existe';
+        } else {
+            header('Location: index.php?erro=1');
+        }
     } else {
         echo 'Erro na execução da consulta, favor entrar em contato com o admin do site';
     }
