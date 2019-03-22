721fb065bae955cd56face53454a084918225441
João Paulo <paulospc21@gmail.com>
2019-03-22 07:22:49 -0300

    criando barras laterais e timeline na home

--- home.php
+++ home.php
@@ -45,25 +45,40 @@
 
 
 <div class="container">
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h4><?= $_SESSION['usuario'] ?></h4>
 
-    <br /><br />
-
-    <div class="col-md-4"></div>
-    <div class="col-md-4">
-        Usuário Autenticado!!
-        <br>
-        <?= $_SESSION['usuario'] ?>
-        <br>
-        <?= $_SESSION['email'] ?>
+                <hr>
+                <div class="col-md-6">
+                    TWEETS <br> 1
+                </div>
+                <div class="col-md-6">
+                    SEGUIDORES <br> 1
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-md-4"></div>
-
-    <div class="clearfix"></div>
-    <br />
-    <div class="col-md-4"></div>
-    <div class="col-md-4"></div>
-    <div class="col-md-4"></div>
-
+    <div class="col-md-6">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="O que está acontecendo agora" maxlength="140">
+                    <span class="input-group-btn">
+                        <button class="btn btn-default">Tweet</button>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h4><a href="">Procurar por pessoas</a></h4>
+            </div>
+        </div>
+    </div>
 </div>
 
 
