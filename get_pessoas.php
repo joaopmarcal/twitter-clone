0aa7064fe80b3415b2fd007c6159ab860219fc4e
João Paulo <paulospc21@gmail.com>
2019-03-25 07:51:55 -0300

    tratando os botões de seguir e deixar de seguir parte 2

--- get_pessoas.php
+++ get_pessoas.php
@@ -14,7 +14,7 @@
     $objDb = new db();
     $link = $objDb->conecta_mysql();
 
-    $sql = "select * from usuarios where usuario like '%$nome_pessoa%' and id <> $id_usuario";
+    $sql = "select u.*, us.* from usuarios as u left join usuarios_seguidores as us on (us.id_usuario = $id_usuario and u.id = us.seguindo_id_usuario) where u.usuario like '%$nome_pessoa%' and u.id <> $id_usuario";
 
     $resultado_id = mysqli_query($link,$sql);
 
@@ -24,8 +24,20 @@
             echo '<a href="#" class="list-group-item">';
                 echo '<strong>' . $registro['usuario'] . '</strong> <small> - ' . $registro['email'] . '</small>';
                 echo '<p class="list-group-item-text pull-right">';
-                    echo '<button type="button" id="btn_seguir_'.$registro['id'].'" class="btn btn-default btn_seguir" data-id_usuario="'.$registro['id'].'">Seguir</button>';
-                    echo '<button type="button" id="btn_deixar_seguir_'.$registro['id'].'" style="display:none" class="btn btn-primary btn_deixar_seguir" data-id_usuario="'.$registro['id'].'">Deixar de Seguir</button>';
+
+                    $esta_seguindo_usuario_sn = isset($registro['id_usuario_seguidor'])  && !empty($registro['id_usuario_seguidor']) ? 'S' : 'N';
+
+                    $btn_seguir_display = 'block';
+                    $btn_deixar_seguir_display = 'block';
+
+                    if($esta_seguindo_usuario_sn == 'N'){
+                        $btn_deixar_seguir_display = 'none';
+                    } else {
+                        $btn_seguir_display = 'none';
+                    }
+
+                    echo '<button type="button" id="btn_seguir_'.$registro['id'].'" style="display: '.$btn_seguir_display.'" class="btn btn-default btn_seguir" data-id_usuario="'.$registro['id'].'">Seguir</button>';
+                    echo '<button type="button" id="btn_deixar_seguir_'.$registro['id'].'" style="display: '.$btn_deixar_seguir_display.'" class="btn btn-primary btn_deixar_seguir" data-id_usuario="'.$registro['id'].'">Deixar de Seguir</button>';
                 echo '</p>';
                 echo '<div class="clearfix"></div>';
             echo '</a>';
